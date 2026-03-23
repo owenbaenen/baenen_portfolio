fba56e715a74cdc6a19966acf75dcbaf7f789ffe
Owen Baenen <ocb3@rice.edu>
2026-03-22 19:18:31 -0500

    Update resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,6 @@ export const personalData = {
   designation: "Engineer",
   description: "Hello! I’m Owen Baenen. I’m a Mechanical Engineering graduate from Rice University with a minor in Computational and Applied Mathematics. My work focuses on the intersection of controls, embedded systems, simulation, and autonomous technologies. Through academic research, simulation development, and hands-on engineering projects, I’ve built experience designing and implementing computational tools, control systems, and hardware-software solutions. I enjoy working in cross-disciplinary environments where mechanical design, software, and algorithms come together to solve complex problems. I’m particularly interested in building intelligent systems that combine modeling, control, and embedded software to enable the next generation of autonomous technologies.",
   email: 'owen.baenen@gmail.com',
-  phone: '+(360) 644-4488',
   address: 'Peoria IL, United States',
   github: 'https://github.com/said7388',
   facebook: 'https://www.facebook.com/abusaid.riyaz/',
@@ -13,5 +12,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "said7388",
-  resume: "https://drive.google.com/file/d/10tm7tMdENwKnLfv_s9bBVrBtLQNH7zOj/view?usp=sharing"
+  resume: "https://drive.google.com/file/d/1puJ3Os5fqtexTds5_dSSoNg95U0z6FwD/view?usp=drive_link"
 }
